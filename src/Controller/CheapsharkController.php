55be4ea384f9432ba7270597cfedd0d17c49d5ac
nfabrecappel <nfabrecappel.pro@gmail.com>
2025-09-02 10:32:09 +0200

    api service creation and game display with twig

--- src/Controller/CheapsharkController.php
+++ src/Controller/CheapsharkController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\CheapsharkApi;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,32 +11,16 @@ use Symfony\Component\Routing\Attribute\Route;
 final class CheapsharkController extends AbstractController
 {
     #[Route('/', name: 'app_cheapshark')]
-    public function index(): Response
+    public function index(CheapsharkApi $api): Response
     {
-        $baseUrl = 'https://www.cheapshark.com/api/1.0/games?id=612';
-
-        $curl = curl_init($baseUrl);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-
-        $response = curl_exec($curl);
-
-        if ($response === false) {
-            throw new Exception("Erreur cURL: " . curl_error($curl));
-        }
-
-        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        curl_close($curl);
-
-        if ($httpCode == 200) {
-            $data = json_decode($response, true);
-            var_dump($data);
-        } else {
-            throw new Exception("Erreur HTTP $httpCode: $response");
-        }
+        $game = $api->getGame(612);
 
         return $this->render('cheapshark/index.html.twig', [
             'controller_name' => 'CheapsharkController',
+            'title' => $game['info']['title'],
+            'image' => $game['info']['thumb'],
+            'cheapest' => $game['cheapestPriceEver']['price'],
+            'deals' => $game['deals'],
         ]);
     }
 }
---
+++ src/Service/CheapsharkApi.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class CheapsharkApi
+{
+    private HttpClientInterface $client;
+    private $baseUrl = 'https://www.cheapshark.com/api/1.0/';
+
+    public function __construct(HttpClientInterface $c) {
+        $this->client = $c;
+    }
+    
+    public function getGame(int $id): array {
+        $response = $this->client->request('GET', $this->baseUrl . 'games', ['query' => ['id' => $id]]);
+
+        return $response->toArray();
+    }
+
+}
