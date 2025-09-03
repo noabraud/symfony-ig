c13ad1e409af03a8fad6241332c919846cf8ee40
nfabrecappel <nfabrecappel.pro@gmail.com>
2025-09-03 10:53:52 +0200

    stores filter working  and  style suppressed from .twig

--- src/Controller/CheapsharkController.php
+++ src/Controller/CheapsharkController.php
@@ -14,7 +14,7 @@ final class CheapsharkController extends AbstractController
     #[Route('/', name: 'app_cheapshark_index')]
     public function index(CheapsharkApi $api): Response
     {
-        $games = $api->getFrom('deals', ['storeID' => '1,7,8,13,25,31', 'pageSize' => '20', 'sortBy' => 'Savings']);
+        $games = $api->getFrom('deals', ['storeID' => $api->getStoresID(['Steam']), 'pageSize' => 20, 'sortBy' => 'Savings']);
 
         return $this->render('cheapshark/index.html.twig', [
             'games' => $games,
@@ -34,7 +34,7 @@ final class CheapsharkController extends AbstractController
             'title' => $game['info']['title'],
             'image' => $game['info']['thumb'],
             'cheapest' => $game['cheapestPriceEver']['price'],
-            'deals' => $game['deals'],
+            'deals' => $api->filterDealsByAllowedStores($game['deals'], $api->getStoresID()),
         ]);
     }
 
@@ -44,7 +44,7 @@ final class CheapsharkController extends AbstractController
         $title = $request->query->get('title');
 
         if ($title) {
-            $games = $api->getFrom('deals', ['title' => $title, 'storeID' => '1,7,8,13,25,31', 'pageSize' => '20', 'sortBy' => 'Metacritic']);
+            $games = $api->getFrom('deals', ['title' => $title, 'storeID' => $api->getStoresID(), 'pageSize' => 20, 'sortBy' => 'Metacritic']);
         }
 
         return $this->render('cheapshark/searchPage.html.twig', [
--- src/Service/CheapsharkApi.php
+++ src/Service/CheapsharkApi.php
@@ -9,15 +9,48 @@ class CheapsharkApi
     private HttpClientInterface $client;
     private $baseUrl = 'https://www.cheapshark.com/api/1.0/';
 
-    public function __construct(HttpClientInterface $c) {
+    private $storesID = [
+        'Steam' => 1,
+        'GOG' => 7,
+        'Origin' => 8,
+        'Uplay' => 13,
+        'Epic Games' => 25,
+        'Blizzard Shop' => 31
+    ];
+
+    public function getStoresID(array $storesName = []): array
+    {
+        if ($storesName && count($storesName) > 0) {
+            $result = [];
+
+            foreach ($storesName as $store) {
+                if (isset($this->storesID[$store])) {
+                    $result[$store] = $this->storesID[$store];
+                }
+            }
+            return $result;
+        }
+        return $this->storesID;
+    }
+
+    public function filterDealsByAllowedStores(array $deals, array $stores): array
+    {
+        $allowedStores = array_flip($stores);
+        return array_filter($deals, fn($deal) => isset($allowedStores[$deal['storeID']]));
+    }
+
+    public function __construct(HttpClientInterface $c)
+    {
         $this->client = $c;
     }
-    
-    public function getFrom(string $field, array $options): array {
-        $response = $this->client->request('GET', $this->baseUrl . $field, ['query' => $options]);
 
+    public function getFrom(string $field, array $options): array
+    {
+        $response = $this->client->request('GET', $this->baseUrl . $field, ['query' => $options]);
         return $response->toArray();
     }
+
+
 
     // public function getGameLookup(int $id): array {
     //     $response = $this->client->request('GET', $this->baseUrl . 'games', ['query' => ['id' => $id]]);
