a79469f4bdbd2513aaf1889132729320da5495a7
nfabrecappel <nfabrecappel.pro@gmail.com>
2025-09-04 11:18:08 +0200

    search form addEventListener and Turbo UX installation

--- src/Service/CheapsharkApi.php
+++ src/Service/CheapsharkApi.php
@@ -6,7 +6,9 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class CheapsharkApi
 {
+    //-- Attributes --\\
     private HttpClientInterface $client;
+
     private $baseUrl = 'https://www.cheapshark.com/api/1.0/';
 
     private $storesID = [
@@ -17,6 +19,12 @@ class CheapsharkApi
         'Epic Games' => 25,
         'Blizzard Shop' => 31
     ];
+
+    //-- Methods --\\
+    public function __construct(HttpClientInterface $c)
+    {
+        $this->client = $c;
+    }
 
     public function getStoresID(array $storesName = []): array
     {
@@ -39,35 +47,9 @@ class CheapsharkApi
         return array_filter($deals, fn($deal) => isset($allowedStores[$deal['storeID']]));
     }
 
-    public function __construct(HttpClientInterface $c)
-    {
-        $this->client = $c;
-    }
-
     public function getFrom(string $field, array $options): array
     {
         $response = $this->client->request('GET', $this->baseUrl . $field, ['query' => $options]);
         return $response->toArray();
     }
-
-
-
-    // public function getGameLookup(int $id): array {
-    //     $response = $this->client->request('GET', $this->baseUrl . 'games', ['query' => ['id' => $id]]);
-
-    //     return $response->toArray();
-    // }
-
-    // public function getDeals(): array {
-    //     $response = $this->client->request('GET', $this->baseUrl . 'deals?storeID=1,7,8,13,25,31&pageSize=10&sortBy=Savings');
-
-    //     return $response->toArray();
-    // }
-
-    // public function getGamesList(string $title): array {
-    //     $response = $this->client->request('GET', $this->baseUrl . 'deals', ['query' => ['title' => $title]]);
-
-    //     return $response->toArray();
-    // }
-
 }
